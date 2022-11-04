46b650a3094d86098bdedbd29348a9b7ecc78868
Groza Sergiu-Cristian <grozasergiu-cristian@Grozas-MacBook-Pro.local>
2022-11-04 10:17:35 +0200

    update test file

--- test/Gtests.cpp
+++ test/Gtests.cpp
@@ -16,7 +16,7 @@ struct ThermostateTest : testing::Test
     }
 };
 
-TEST_F(ThermostateTest,ThermostateInitialState)
+TEST_F(ThermostateTest,ThermostateInitialTemp)
 {
     EXPECT_EQ(21.f,thermo->get_temperature());
 }
@@ -25,6 +25,33 @@ TEST_F(ThermostateTest,ThermostateInitialState)
 {
     EXPECT_EQ(State::stop,thermo->get_thernostate_state());
 }
+
+TEST_F(ThermostateTest,ThermostateSetMaxTrue)
+{
+    EXPECT_EQ(true,thermo->set_limit_max(33.f));
+}
+
+TEST_F(ThermostateTest,ThermostateSetMaxFalse)
+{
+    EXPECT_EQ(false,thermo->set_limit_max(9.f));
+}
+
+TEST_F(ThermostateTest,ThermostateSetMinTrue)
+{
+    EXPECT_EQ(true,thermo->set_limit_min(9.f));
+}
+
+TEST_F(ThermostateTest,ThermostateSetMinFalse)
+{
+    EXPECT_EQ(false,thermo->set_limit_max(33.f));
+}
+
+TEST_F(ThermostateTest,ThermostateSetTemp)
+{
+    thermo->set_temperature(30.f);
+    EXPECT_EQ(30.f,thermo->get_temperature());
+}
+
 
 int main(int argc, char* argv[])
 {
